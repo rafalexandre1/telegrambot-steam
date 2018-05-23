a4cabddb2525e3d09292cb9c9ef5d2dfade7f1ce
Rafael Alexandre <rafael.alexandre.tm@gmail.com>
2018-05-22 21:19:16 -0300

    remoção da versão do python

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo pip3 install -r requirements.txt
-sudo python3.6 setup.py install
+sudo python3 setup.py install
 
 sudo chmod +x python_init.py start-bot.sh
 sudo chown $USER:$USER python_init.py start-bot.sh
